a304a06b162ee9db8d8eb30b05c2b1098720903f
Tucker Chapman <tucker.r.chapman@gmail.com>
2018-09-12 21:35:20 -0400

    Add job category component

---
+++ src/components/JobCategory.vue
@@ -0,0 +1,20 @@
+<template>
+  <span class="category">{{category}}</span>
+</template>
+
+<script>
+export default {
+  props: {
+    category: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style>
+    .category {
+        padding: 10px;
+    }
+</style>
--- src/components/JobListItem.vue
+++ src/components/JobListItem.vue
@@ -1,12 +1,26 @@
 <template>
-    <li>
-        <span>Title: </span>{{job.properties.title}}<br />
-        <span>Location: </span>{{job.properties.location}}<br />
-    </li>
+    <div class="job-listing">
+        <div class="title">Title: {{job.properties.title}}</div>
+        <div class="location">Location:
+            <span v-if="job.properties.location">{{job.properties.location}}</span>
+            <span v-else>No office location</span>
+        </div>
+        <div class="categories">
+            <JobCategory
+              v-for="category in job.properties.category"
+              :category="category"
+            />
+        </div>
+    </div>
 </template>
 
 <script>
+import JobCategory from "./JobCategory.vue"
+
 export default {
+  components: {
+      JobCategory
+  },
   props: {
     job: {
       type: Object,
@@ -15,3 +29,11 @@ export default {
   }
 }
 </script>
+
+<style>
+    .job-listing {
+        text-align: left;
+        padding: 20px;
+
+    }
+</style>
