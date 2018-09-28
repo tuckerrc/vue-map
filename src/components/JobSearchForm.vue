fb8d60f44b241ab30958d8ea1f84069472f0acea
Tucker Chapman <tucker.r.chapman@gmail.com>
2018-09-27 21:50:38 -0400

    Update form to use tailwind classes

--- src/components/JobSearchForm.vue
+++ src/components/JobSearchForm.vue
@@ -1,34 +1,39 @@
 <template>
   <form
+    class="my-4"
     id="jobSearch"
     v-on:submit.prevent="handleSubmit"
   >
-    <input
-      id="search"
-      placeholder="Search Jobs"
-      class="input"
-      name="search"
-      type="text"
-      v-model="search"
-    >
-    <input
-      id="location"
-      placeholder="Location"
-      class="input"
-      name="location"
-      type="text"
-      v-model="location"
-    >
-    <label for="remote">Remote? </label>
-    <input
-      id="remote"
-      type="checkbox"
-      v-model="remote"
-    >
-    <input
-      type="submit"
-      value="Search"
-    >
+    <div class="flex flex-wrap">
+      <input
+        id="search"
+        placeholder="Search Jobs"
+        class="shadow appearance-none border rounded w-30 mb-4 py-2 px-3 mr-2 text-grey-darker leading-tight focus:outline-none focus:shadow-outline"
+        name="search"
+        type="text"
+        v-model="search"
+      >
+      <input
+        id="location"
+        placeholder="Location"
+        class="shadow appearance-none border rounded w-30 mb-4 py-2 px-3 mr-2 text-grey-darker leading-tight focus:outline-none focus:shadow-outline"
+        name="location"
+        type="text"
+        v-model="location"
+      >
+      <span class="m-2 display-inline">
+        <label for="remote">Remote? </label>
+        <input
+          id="remote"
+          type="checkbox"
+          v-model="remote"
+        >
+      </span>
+      <button
+        class="bg-blue hover:bg-blue-dark text-white font-bold mb-4 py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        type="submit"
+      >Search</button>
+    </div>
   </form>
 </template>
 
